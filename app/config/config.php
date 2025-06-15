48754f068c4fd7f0799eb9ed1125d8391a5522d3
yegane <norouzi.yegane@gmail.com>
2025-06-15 11:15:18 +0330

    Resolve config enviroment

--- app/config/config.php
+++ app/config/config.php
@@ -1,21 +1,44 @@
 <?php
-define("APPROOT", dirname(__DIR__));
-define("UploadImage", dirname(dirname(__DIR__)));
 
+// Base paths
+$config['paths'] = [
+    'app_root' => dirname(__DIR__),
+    'upload_dir' => dirname(dirname(__DIR__))
+];
 
-// define("URLROOT", "https://pfsh.ir");
-define("URLROOT", "http://localhost/ak-new");
+// Environment settings
+$config['environment'] = 'development'; // or 'production'
 
-// config database ...... 
-// define("DB__Host", "localhost");
-// define("DB__USER", "pfsh_ak1");
-// define("DB__PASS", "Sg.yhoG4[4X}");
-// define("DB__NAME", "pfsh_ak");
+// URL settings
+$config['url'] = [
+    'development' => 'http://localhost/ak-new',
+    'production' => 'https://pfsh.ir'
+];
 
+// Database settings
+$config['database'] = [
+    'development' => [
+        'host' => 'localhost',
+        'username' => 'root',
+        'password' => '',
+        'database' => 'ak'
+    ],
+    'production' => [
+        'host' => 'localhost',
+        'username' => 'pfsh_ak1',
+        'password' => 'Sg.yhoG4[4X}',
+        'database' => 'pfsh_ak'
+    ]
+];
 
-//local database 
+// Get current environment settings
+$currentEnv = $config['environment'];
 
-define("DB__Host", "localhost");
-define("DB__USER", "root");
-define("DB__PASS", "");
-define("DB__NAME", "ak");
+// Define constants for backward compatibility
+define('APPROOT', $config['paths']['app_root']);
+define('UploadImage', $config['paths']['upload_dir']);
+define('URLROOT', $config['url'][$currentEnv]);
+define('DB__Host', $config['database'][$currentEnv]['host']);
+define('DB__USER', $config['database'][$currentEnv]['username']);
+define('DB__PASS', $config['database'][$currentEnv]['password']);
+define('DB__NAME', $config['database'][$currentEnv]['database']);
